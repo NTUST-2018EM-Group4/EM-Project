23c82661e084d99a26d3bc3586ff029a68e7054c
Jeffery Ho <chiachun2491@gmail.com>
2019-03-26 10:52:18 +0800

    Defined Matrix Struct

--- Project1/WindowsFormsApplication_cpp/DataManager.h
+++ Project1/WindowsFormsApplication_cpp/DataManager.h
@@ -3,6 +3,7 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+
 //定義向量資料結構
 struct Vector
 {
@@ -19,6 +20,17 @@ struct Vector
 	const double operator * (const Vector& Vb);
 	friend const  Vector operator * (const Vector& Va, const Vector& Vb);
 };
+
+//Defined Struct Matrix
+struct Matrix
+{
+	std::string Name;
+	std::vector<std::vector<double>> Data;
+
+	Matrix();
+	Matrix(std::vector<std::vector<double>> data);
+};
+
 //定義控管資料class
 class DataManager
 {
@@ -27,14 +39,26 @@ private:
 	std::vector<Vector> Vectors;
 	//紀錄向量ID，用於控管
 	int  VectorVariableIndex;
+
+	//Store Matrix Data
+	std::vector<Matrix> Matrices;
+	//Record Matrix ID
+	int MatrixVariableIndex;
+
 	//紀錄檔案路徑名稱
 	std::string FileName;
 public:
 	DataManager();
+
 	//讀取向量資料
 	bool LoadVectorData();
 	//取得向量資料
 	std::vector<Vector> GetVectors();
+
+	//Read Matrix Data
+	bool LoadMatrixData();
+	//Get Matrix Data
+	std::vector<Matrix> GetMatrices();
 	//設置檔案路徑名稱
 	void SetFileName(std::string fileName);
 };
