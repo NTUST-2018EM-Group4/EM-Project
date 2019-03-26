309ddcadd133804361b1cf2cfe17e7b1a9b80ca6
Jeffery Ho <chiachun2491@gmail.com>
2019-03-26 22:27:56 +0800

    Change code Location .h to .cpp
    Fxxking trash example code

--- Project1/WindowsFormsApplication_cpp/WindowsForm.h
+++ Project1/WindowsFormsApplication_cpp/WindowsForm.h
@@ -13,42 +13,6 @@ namespace WindowsFormsApplication_cpp {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
-	int priority(char op) {
-		switch (op) {
-		case '+': case '-': return 1;
-		case '*': case '/': return 2;
-		default:            return 0;
-		}
-	}
-
-	String^ printVector(String^ s , const Vector& v)
-	{
-		s += "[";
-		for (int i = 0; i < v.Data.size(); i++)
-		{
-			s += v.Data[i].ToString();
-			if (i != v.Data.size() - 1)
-				s += ",";
-		}
-		s += "]" + Environment::NewLine;
-		return s;
-	}
-
-	int findVector( std::string name, const std::vector<Vector>& v)
-	{
-		std::string temp;
-		//MarshalString(name, temp);
-
-		//透過for迴圈，從向量資料中找出對應變數
-		for (unsigned int i = 0; i < v.size(); i++)
-		{
-			//若變數名稱與指令變數名稱符合
-			if (name == v[i].Name)
-				return i;
-		}
-		return -1;
-	}
-
 	/// <summary>
 	/// WindowsForm 的摘要
 	/// </summary>
@@ -75,35 +39,26 @@ namespace WindowsFormsApplication_cpp {
 		}
 
 	private: System::Windows::Forms::MenuStrip^  menuStrip2;
+			 System::Windows::Forms::ToolStripMenuItem^  loadVectorToolStripMenuItem;
+			 System::Windows::Forms::ToolStripMenuItem^  loadMatrixToolStripMenuItem;
+			 System::Windows::Forms::OpenFileDialog^  openFileDialog1;
+			 System::Windows::Forms::OpenFileDialog^  openFileDialog2;
+			 System::Windows::Forms::GroupBox^  groupBox2;
+			 System::Windows::Forms::TextBox^  Output;
+			 System::Windows::Forms::GroupBox^  groupBox1;
+			 System::Windows::Forms::TextBox^  Input;
+			 System::Windows::Forms::ListBox^  VectorList;
+			 System::Windows::Forms::ListBox^  MatrixList;
+			 System::Windows::Forms::TableLayoutPanel^  tableLayoutPanel1;
+			 System::Windows::Forms::GroupBox^  groupBox3;
+			 System::Windows::Forms::GroupBox^  groupBox4;
 
-	private: System::Windows::Forms::ToolStripMenuItem^  loadVectorToolStripMenuItem;
-	private: System::Windows::Forms::ToolStripMenuItem^  loadMatrixToolStripMenuItem;
-
-	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
-	private: System::Windows::Forms::OpenFileDialog^  openFileDialog2;
-
-	protected:
-
-	private:
 		/// <summary>
 			DataManager* dataManager;
 			String^ userInput;
 			int lastInputLength;
-	private: System::Windows::Forms::GroupBox^  groupBox2;
-	private: System::Windows::Forms::TextBox^  Output;
-	private: System::Windows::Forms::GroupBox^  groupBox1;
-	private: System::Windows::Forms::TextBox^  Input;
-	private: System::Windows::Forms::ListBox^  VectorList;
-	private: System::Windows::Forms::ListBox^  MatrixList;
-	private: System::Windows::Forms::TableLayoutPanel^  tableLayoutPanel1;
-	private: System::Windows::Forms::GroupBox^  groupBox3;
-	private: System::Windows::Forms::GroupBox^  groupBox4;
-
-
-
-			/// </summary>
+		/// </summary>
 			System::ComponentModel::Container ^components;
-
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -322,406 +277,21 @@ namespace WindowsFormsApplication_cpp {
 		}
 #pragma endregion
 
-private: System::Void WindowsForm_Load(System::Object^  sender, System::EventArgs^  e) {
-}
+	private: 
+		System::Void WindowsForm_Load(System::Object^  sender, System::EventArgs^  e) {}
 
-private: System::Void Input_TextChanged(System::Object^  sender, System::EventArgs^  e)
-{
-	//當Input textbox中的輸入改變時，便會進入此函式
-	//取得向量資料
-	std::vector<Vector> vectors = dataManager->GetVectors();
-	//判斷輸入自元為'\n'，並防止取到字串-1位置
-	if (Input->Text->Length-1 >= 0 &&Input->Text[Input->Text->Length - 1] == '\n')
-	{
-		//將使用者輸入字串(在userInput中)，依空白作切割
-		array<String^> ^userCommand = userInput->Split(' ');
-		//字串比較，若指令為"print"的情況
-		if (userCommand[0] == "print")
-		{
-			//定義輸出暫存
-			String^ outputTemp = "";
-			std::string nameTemp;
-			MarshalString(userCommand[1], nameTemp);
-			int index = findVector(nameTemp, vectors);
-			if (index != -1)
-			{
-				outputTemp = printVector(outputTemp, vectors[index]);
-				Output->Text += gcnew String(vectors[index].Name.c_str()) + " = " + outputTemp;
-			}
-			else
-				Output->Text += "-Vector not found-" + Environment::NewLine;
-		}
+		System::Void openFileDialog1_FileOk(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e);
+		System::Void loadVectorToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e);
+		System::Void loadMatrixToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e);
+		System::Void openFileDialog2_FileOk(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e);
 
-		else if (userCommand[0] == "cal")
-		{
-			//infix to postfix
+		System::Void Input_TextChanged(System::Object^  sender, System::EventArgs^  e);
+	};
 
-			//若運算式中有空格先合併成無空格版
-			for (int i = 2; i < userCommand->Length; i++)
-			{
-				userCommand[1] += userCommand[i];
-			}
-			//將無空格版處理成空格版
-			//定義輸出暫存
-			String^ infixTemp = "";
-			int begin = 0;
-			for (int i = 0; i < userCommand[1]->Length; i++)
-			{
-				if (userCommand[1][i] == '(' ||
-					userCommand[1][i] == ')' ||
-					userCommand[1][i] == '+' ||
-					userCommand[1][i] == '-' ||
-					userCommand[1][i] == '*')
-				{
-					if (i - 1 >= 0)
-					{
-						infixTemp += userCommand[1]->Substring(begin, (i - 1 - begin + 1));
-						infixTemp += " ";
-					}
-					infixTemp += userCommand[1]->Substring(i, 1);
-					infixTemp += " ";
-					begin = i + 1;
-				}
-				else if (i == userCommand[1]->Length - 1)
-				{
-					infixTemp += userCommand[1]->Substring(begin, (i - begin + 1));
-					infixTemp += " ";
-				}
-			}
-			//去除最後一個空格
-			if (infixTemp[infixTemp->Length - 1] == ' ')
-			{
-				infixTemp = infixTemp->Remove(infixTemp->Length - 1, 1);
-			}
-			//將處理完的字串依空白作切割
-			array<String^> ^infixFormula = infixTemp->Split(' ');
-			String^ stack = " ";
-
-			String^ postfix = "";
-			int top = 0;
-			for (int i = 0; i < infixFormula->Length; i++)
-			{
-
-				if (infixFormula[i] == "(")
-				{
-					// 運算子堆疊
-					stack = stack->Insert(++top, infixFormula[i]);
-				}
-				else if (infixFormula[i] == "+" ||
-					infixFormula[i] == "-" ||
-					infixFormula[i] == "*")
-				{
-					while (priority(stack[top]) >= priority(infixFormula[i][0]))
-					{
-						postfix += stack[top--];
-						postfix += " ";
-					}
-					stack = stack->Insert(++top, infixFormula[i]); // 存入堆疊
-				}
-				else if (infixFormula[i] == ")")
-				{
-					while (stack[top] != '(') { // 遇 ) 輸出至 (
-						postfix += stack[top--];
-						postfix += " ";
-					}
-					top--;  // 不輸出 (
-				}
-				else
-				{
-					// 運算元直接輸出
-					postfix += infixFormula[i];
-					postfix += " ";
-				}
-			}
-			while (top > 0) {
-				postfix += stack[top--];
-				postfix += " ";
-			}
-			//去除最後一個空格
-			if (postfix[postfix->Length - 1] == ' ')
-			{
-				postfix = postfix->Remove(postfix->Length - 1, 1);
-			}
-			//將處理完的字串(postfix)依空白作切割存到array
-			array<String^> ^postfixArray = postfix->Split(' ');
-
-			//將array(postfixArray)轉成List
-			Generic::List<String^> ^postfixFormula = gcnew Generic::List<String^> ();
-			for (int i = 0; i < postfixArray->Length; i++)
-			{
-				//Avoid " " bug
-				if (postfixArray[i] != "")
-				{
-					postfixFormula->Add(postfixArray[i]);
-				}
-			}
-
-#ifdef DEBUG
-			Output->Text += "postfix = " + postfix + Environment::NewLine;
-#endif // DEBUG
-
-			Vector ans, Va, Vb;
-			std::stack<Vector> calStack;
-			bool dimFlag, foundFlag;
-			for (int i = 0; i < postfixFormula->Count; i++)
-			{
-				dimFlag = 0;
-				foundFlag = 0;
-				Va = Vector();
-				Vb = Vector();
-				if (postfixFormula[i] == "+" || postfixFormula[i] == "-" || postfixFormula[i] == "*")
-				{
-					std::string temp;
-					double scalar;
-
-					//check scalar or not
-					System::String^ Temp = postfixFormula[i - 2];
-					MarshalString(Temp, temp);
-					if (temp[0] != '$')
-						scalar = strtod(temp.c_str(), NULL);
-
-					Temp = postfixFormula[i - 1];
-					MarshalString(Temp, temp);
-					if (temp[0] != '$')
-						scalar = strtod(temp.c_str(), NULL);
-
-					//find vector to calculate
-					std::string nameTemp;
-					MarshalString(postfixFormula[i - 2], nameTemp);
-					int index = findVector(nameTemp, vectors);
-					if (index != -1)
-						Va = vectors[index];
-
-					MarshalString(postfixFormula[i - 1], nameTemp);
-					index = findVector(nameTemp, vectors);
-					if (index != -1)
-						Vb = vectors[index];
-
-					//從算式stack中獲得運算子
-					if (Vb.Name == "" && !calStack.empty())
-					{
-						Vb = calStack.top();
-						calStack.pop();
-					}
-
-					if (Va.Name == "" && !calStack.empty())
-					{
-						Va = calStack.top();
-						calStack.pop();
-					}
-
-					//found check
-					if (!Va.Data.empty() && !Vb.Data.empty())
-						foundFlag = 1;
-					else if (Va.Data.empty() && Vb.Data.empty()) break;
-
-					//dimesion check
-					if (Va.dimCheck(Vb)) {
-						dimFlag = 1;
-
-						if (postfixFormula[i] == "+")
-						{
-							//call Addition
-#ifdef DEBUG
-							Output->Text += "Addition called" + Environment::NewLine;
-#endif // DEBUG
-							ans = Va + Vb;
-						}
-						else if (postfixFormula[i] == "-")
-						{
-							//call Subtraction
-#ifdef DEBUG
-							Output->Text += "Subtraction called" + Environment::NewLine;
-#endif // DEBUG
-							ans = Va - Vb;
-						}
-						else if (postfixFormula[i] == "*")
-						{
-							//call dot
-#ifdef DEBUG
-							Output->Text += "Dot called" + Environment::NewLine;
-#endif // DEBUG
-							ans = Va * Vb;
-						}
-						//push into calStack
-						calStack.push(ans);
-
-						postfixFormula->RemoveAt(i - 2);
-						postfixFormula->RemoveAt(i - 2);
-						i -= 2;
-
-					}
-					else
-					{
-						if (Va.Data.empty() || Vb.Data.empty() || \
-							(!Va.Data.empty() && !Vb.Data.empty()))
-						{
-							if (Va.Data.empty() && postfixFormula[i - 2][0] == '$' || \
-								Vb.Data.empty() && postfixFormula[i - 1][0] == '$')
-								continue;
-							dimFlag = 1;
-							foundFlag = 1;
-							//call scalar
-#ifdef DEBUG
-							Output->Text += "Scalar called" + Environment::NewLine;
-#endif // DEBUG
-							if (Va.Data.size() < Vb.Data.size())
-							{
-								if (!Va.Data.empty())
-									scalar = Va.Data[0];
-								ans = scalar * Vb;
-							}
-							else
-							{
-								if (!Vb.Data.empty())
-									scalar = Vb.Data[0];
-								ans = scalar * Va;
-							}
-							//push into calStack
-							calStack.push(ans);
-							postfixFormula->RemoveAt(i - 2);
-							postfixFormula->RemoveAt(i - 2);
-							i -= 2;
-						}
-						else break;
-					}
-				}
-			}
-
-			if (!foundFlag)
-				Output->Text += "-Vector not found-" + Environment::NewLine;
-			else if (!dimFlag)
-				Output->Text += "-Dimension not same-" + Environment::NewLine;
-			else
-			{
-				//格式無誤，輸出結果
-
-				String^ outputTemp;
-				//將輸出資料存入暫存
-				ans = calStack.top();
-				calStack.pop();
-				outputTemp = printVector(outputTemp, ans);
-				//輸出暫存資訊
-				Output->Text += gcnew String(userCommand[1] + " = " + outputTemp) + Environment::NewLine;
-			}
-		}
-
-		else if (userCommand[0] == "func")
-		{
-			Vector Va, Vb;
-			bool foundFlag = 0;
-			if (userCommand[1] == "Norm\(" && userCommand[3] == ")")
-			{
-#ifdef DEBUG
-				Output->Text += "Norm called" + Environment::NewLine;
-#endif // DEBUG
-				for (unsigned int i = 0; i < vectors.size(); i++)
-				{
-					if (userCommand[2] == gcnew String(vectors[i].Name.c_str()))
-					{
-						Va = vectors[i];
-						foundFlag = 1;
-						break;
-					}
-				}
-				if (foundFlag)
-				{
-					double ans = Va.Norm();
-					Output->Text += "Norm(" + userCommand[2] + ") = " + ans + Environment::NewLine;
-				}
-			}
-			else if (userCommand[1] == "Normal(" && userCommand[3] == ")")
-			{
-#ifdef DEBUG
-				Output->Text += "Normalization called" + Environment::NewLine;
-#endif // DEBUG
-				for (unsigned int i = 0; i < vectors.size(); i++)
-				{
-					if (userCommand[2] == gcnew String(vectors[i].Name.c_str()))
-					{
-						Va = vectors[i];
-						foundFlag = 1;
-						break;
-					}
-				}
-				if (foundFlag)
-				{
-					Vector ans = Va.Normal();
-					Output->Text += "Normal(" + userCommand[2] + ") = ";
-					//call print
-				}
-			}
-			else
-				Output->Text += "-Function not found-" + Environment::NewLine;
-
-			if (!foundFlag)
-				Output->Text += "-Vector not found-" + Environment::NewLine;
-		}
-		//反之則判斷找不到指令
-		else
-		{
-			Output->Text += "-Command not found-" + Environment::NewLine;
-		}
-		userInput = "";
-	}
-	else
-	{
-		//將使用者輸入字串(在Text box中)，依'\n'作切割
-		array<String^> ^userCommand = Input->Text->Split('\n');
-		//並將最後一行，作為目前使用者輸入指令
-		userInput = userCommand[userCommand->Length-1];
-	}
-
-}
-private: System::Void openFileDialog1_FileOk(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e)
-{
-	//在Dialog按下OK便會進入此函式
-	//字串轉制string^ to string
-	std::string tempFileName;
-	MarshalString(openFileDialog1->FileName, tempFileName);
-	//將檔案路徑名稱傳入dataManager
-	dataManager->SetFileName(tempFileName);
-	//從讀取讀取向量資料
-	if (dataManager->LoadVectorData())
-	{
-		//將VectorList中項目先做清除
-		VectorList->Items->Clear();
-		//取得所有向量資料
-		std::vector<Vector> vectors = dataManager->GetVectors();
-
-		for (unsigned int i = 0; i < vectors.size(); i++)
-		{
-			//將檔案名稱存入暫存
-			std::string tempString = vectors[i].Name;
-			//將輸出格式存入暫存
-			tempString += " [";
-			//將輸出資料存入暫存
-			for (unsigned int j = 0; j<vectors[i].Data.size(); j++)
-			{
-				std::string scalarString = std::to_string(vectors[i].Data[j]);
-				tempString += scalarString.substr(0, scalarString.size() - 5);
-				if (j != vectors[i].Data.size() - 1)
-					tempString += ",";
-			}
-			//將輸出格式存入暫存
-			tempString += "]";
-			//將項目加入VectorList中
-			VectorList->Items->Add(gcnew String(tempString.c_str()));
-		}
-		Output->Text += "-Vector datas have been loaded-" + Environment::NewLine;
-	}
-}
-
-private: System::Void loadVectorToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e)
-{
-	//開啟Dialog
-	openFileDialog1->ShowDialog();
-}
-
-private:
-	System::Void loadMatrixToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e);
-	System::Void openFileDialog2_FileOk(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e);
-
-
-};
+	//
+	// function
+	//
+	int priority(char op);
+	String^ printVector(String^ s, const Vector& v);
+	int findVector(std::string name, const std::vector<Vector>& v);
 }
