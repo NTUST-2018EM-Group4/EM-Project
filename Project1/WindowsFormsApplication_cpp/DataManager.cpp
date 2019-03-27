103c70ac2804cc3ed4b9be4ce4b29ab2290f8acc
Jeffery Ho <chiachun2491@gmail.com>
2019-03-27 14:41:56 +0800

    Fixed bug of the read vector
    Fxxking trash exmple code, it's very useless.

--- Project1/WindowsFormsApplication_cpp/DataManager.cpp
+++ Project1/WindowsFormsApplication_cpp/DataManager.cpp
@@ -19,62 +19,43 @@ bool DataManager::LoadVectorData()
 	}
 	else
 	{
-		//標記當前讀取向量ID
-		int currentLoadVectorID = 0;
-		//定義向量資料暫存變數
-		std::vector<double> tempVectorData;
 		//定義讀取檔案字串暫存變數
-		std::string tempSring;
-		//從檔案讀取字串，解析掉向量總數
-		fin >> tempSring;
+		std::string tempString;
+		//從檔案讀取字串，解析掉Vector總數(目前沒用到)
+		fin >> tempString;
 
 		//執行讀檔迴圈，並在讀到檔案結尾時結束
 		while (!fin.eof())
 		{
 			//從檔案讀取字串
-			fin >> tempSring;
-			//解析到向量標記"V"
-			if (tempSring == "V")
-			{
-				if (currentLoadVectorID != 0)
-				{
-					//定義暫存向量資料結構
-					Vector tempVector;
-					//存入向量資料
-					tempVector.Data = tempVectorData;
-					//定義向量變數名稱，依VectorVariableIndex變數作名稱的控管
-					std::string vectorVariableTemp = "$v" + std::to_string(VectorVariableIndex);
-					//存入向量變數名稱
-					tempVector.Name = vectorVariableTemp;
-					//存入向量
-					Vectors.push_back(tempVector);
-					//遞增VectorVariableIndex，以確保變數名稱不重複
-					VectorVariableIndex++;
-					//清除向量資料暫存
-					tempVectorData.clear();
-				}
-				//遞增currentLoadVectorID，標記到當前讀取向量ID
-				currentLoadVectorID++;
-				//從檔案讀取字串，解析掉向量維度
-				fin >> tempSring;
-			}
-			else
-			{
-				//讀取向量資料，並將string轉為double
-				double value;
-				value = (double)strtod(tempSring.c_str(), NULL);
-				//將向量資料存入暫存
-				tempVectorData.push_back(value);
-			}
+			fin >> tempString;
 
+			//解析到Vector標記"V"
+			if (tempString == "V")
+			{
+				Vector tempVector;
+
+				int dim;
+				// store dim of vector
+				fin >> dim;
+				for (int i = 0; i < dim; i++)
+				{
+					double tempValue;
+					fin >> tempValue;
+					tempVector.Data.push_back(tempValue);
+				}
+				// set Vector name as $v[Index]
+				tempVector.Name = "$v" + std::to_string(VectorVariableIndex);
+				// add to Vector List of DataManager
+				Vectors.push_back(tempVector);
+				// Index ++
+				VectorVariableIndex++;
+				// clean temp data to avoid duplicate data bug
+				tempVector.Data.clear();
+				tempVector.Name = "";
+				tempString = "";
+			}
 		}
-		//讀入輸入檔案中最後一個向量資訊
-		Vector tempVector;
-		tempVector.Data = tempVectorData;
-		std::string vectorVariableTemp = "$v" + std::to_string(VectorVariableIndex);
-		tempVector.Name = vectorVariableTemp;
-		Vectors.push_back(tempVector);
-		VectorVariableIndex++;
 		//讀取成功回傳false
 		return true;
 	}
@@ -94,18 +75,18 @@ bool DataManager::LoadMatrixData()
 	else
 	{
 		//定義讀取檔案字串暫存變數
-		std::string tempSring;
+		std::string tempString;
 		//從檔案讀取字串，解析掉Matrix總數(目前沒用到)
-		fin >> tempSring;
+		fin >> tempString;
 
 		//執行讀檔迴圈，並在讀到檔案結尾時結束
 		while (!fin.eof())
 		{
 			//從檔案讀取字串
-			fin >> tempSring;
+			fin >> tempString;
 
 			//解析到Matrix標記"M"
-			if (tempSring == "M")
+			if (tempString == "M")
 			{
 				Matrix tempMatrix;
 
@@ -136,6 +117,7 @@ bool DataManager::LoadMatrixData()
 				// clean temp data to avoid bug
 				tempMatrix.Data.clear();
 				tempMatrix.Name = "";
+				tempString = "";
 			}
 		}
 		//讀取成功回傳false
