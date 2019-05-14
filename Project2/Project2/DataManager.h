97e02f9375e1e7004ae3929e1430d076001292ff
Jeffery Ho <chiachun2491@gmail.com>
2019-05-14 21:19:21 +0800

    Updated DataManager

--- Project2/Project2/DataManager.h
+++ Project2/Project2/DataManager.h
@@ -4,42 +4,25 @@
 #include <fstream>
 #include <string>
 
-#include "Vector.h"
-#include "Matrix.h"
 
-//定義控管資料class
 class DataManager
 {
-private:	
-	//儲存向量資料
-	std::vector<Vector> Vectors;
-	//紀錄向量ID，用於控管
-	int  VectorVariableIndex;
+private:
 
-	//Store Matrix Data
-	std::vector<Matrix> Matrices;
-	//Record Matrix ID
-	int MatrixVariableIndex;
-
-	//紀錄檔案路徑名稱
+	std::vector<std::string> Equations;
+	int EquationIndex;
 	std::string FileName;
+
 public:
+	//
+	// Constructor
+	//
 	DataManager();
 
-	//讀取向量資料
-	bool LoadVectorData();
-	//取得向量資料
-	std::vector<Vector> GetVectors();
-	// find vector index
-	int findVector(std::string name);
-
-	//Read Matrix Data
-	bool LoadMatrixData();
-	//Get Matrix Data
-	std::vector<Matrix> GetMatrices();
-	// find matrix index
-	int findMatrix(std::string name);
-
-	//設置檔案路徑名稱
+	//
+	// Member Function
+	//
+	bool LoadEquationData();
+	std::vector<std::string> GetEquations();
 	void SetFileName(std::string fileName);
 };
